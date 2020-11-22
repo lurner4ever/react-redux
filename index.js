fb7bba51adf2f9490c21f89a25a794bbbbeebe76
lurner4ever <40304637+lurner4ever@users.noreply.github.com>
2020-11-22 18:10:48 +0530

    Regular redux operation

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("hello world");
+const redux = require("redux");
+
+const BUY_CAKE = "BUY_CAKE";
+const createStore = redux.createStore;
+
+// action creator
+function buyCake() {
+  // action
+  return {
+    type: BUY_CAKE,
+    info: "hello Redux",
+  };
+}
+
+// reducer
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// 1.creating store
+const store = createStore(reducer);
+// 2.getting state
+console.log("Initial state", store.getState());
+// 4.adding listeners
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+// 3.updating the state via dispatch()
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+// 4.unsubscribe
+unsubscribe();
