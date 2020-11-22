c3d5d2065aede22c534c7126984e60a0dd4c8cbc
lurner4ever <40304637+lurner4ever@users.noreply.github.com>
2020-11-22 18:33:49 +0530

    Add combine reducer

--- index.js
+++ index.js
@@ -1,36 +1,68 @@
 const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
-const createStore = redux.createStore;
+const BUY_ICECREAM = "BUY_ICECREAM";
 
-// action creator
+// cake action creator
 function buyCake() {
-  // action
+  // cake action
   return {
     type: BUY_CAKE,
-    info: "hello Redux",
   };
 }
 
-// reducer
-const initialState = {
-  numOfCakes: 10,
+// Icecream action creator
+function buyIcecream() {
+  // Icecream action
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
+// reducers
+// cake reducer
+const cakeInitialState = {
+  noOfCakes: 10,
 };
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = cakeInitialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
-        numOfCakes: state.numOfCakes - 1,
+        noOfCakes: state.noOfCakes - 1,
       };
     default:
       return state;
   }
 };
 
+// icecream reducer
+const icecreamInitialState = {
+  noOfIcecreams: 10,
+};
+const icecreamReducer = (state = icecreamInitialState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        noOfIcecreams: state.noOfIcecreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// combine reducers
+const rootReducer = combineReducers({
+  cakeReducer,
+  icecreamReducer,
+});
+
 // 1.creating store
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 // 2.getting state
 console.log("Initial state", store.getState());
 // 4.adding listeners
@@ -38,9 +70,12 @@ const unsubscribe = store.subscribe(() =>
   console.log("updated state", store.getState())
 );
 // 3.updating the state via dispatch()
-store.dispatch(buyCake());
-store.dispatch(buyCake());
-store.dispatch(buyCake());
+store.dispatch(buyCake()); //updating state by dispatch
+store.dispatch(buyCake()); //updating state by dispatch
+store.dispatch(buyCake()); //updating state by dispatch
+store.dispatch(buyIcecream()); //updating state by dispatch
+store.dispatch(buyIcecream()); //updating state by dispatch
+store.dispatch(buyIcecream()); //updating state by dispatch
 
 // 4.unsubscribe
 unsubscribe();
